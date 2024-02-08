393f5e045e8bbba5b91d8f374907119848f47425
jv <joaovictor.iteck@gmail.com>
2024-02-08 10:14:25 -0300

    random weapons

--- health_bar.py
+++ health_bar.py
@@ -41,6 +41,7 @@ class HealthBar:
         remaining_bars = round(self.current_value / self.max_value * self.length)
         lost_bars = self.length - remaining_bars
         print(f"{self.entity.name}'s HEALTH: {self.entity.health}/{self.entity.health_max}")
+        
         print(f"{self.barrier}"
               f"{self.color if self.is_colored else ''}"
               f"{remaining_bars * self.symbol_remaining}"
